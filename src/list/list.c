353a73b55db294a28ddb433163c9589806cffa69
brenn0219 <brennoaugusto2109@gmail.com>
2023-12-24 16:55:09 -0300

    feat: linked lists

---
+++ src/list/list.c
@@ -0,0 +1,77 @@
+#include "list.h"
+
+void list_init(List *list, void (*destroy) (void *data)) {
+    list->size = 0;
+    list->destroy = destroy;
+    list->head = NULL;
+    list->tail = NULL;
+}
+
+void list_destroy(List *list) {
+    void *data;
+
+    while (list_size(list) > 0)  {
+        if (list_rem_next(list, NULL, (void **) &data) == 0 && list->destroy != NULL)
+            list->destroy(data);
+    }
+    
+    memset(list, 0, sizeof(List));
+}
+
+int list_ins_next(List *list, ListElmt *element, const void *data) {
+    ListElmt *new_element;
+
+    if (new_element = (ListElmt *) malloc(sizeof(ListElmt)) == NULL)
+        return -1;
+    
+    new_element->data = data;
+
+    if (element == NULL) {
+        if (list_size(list) == 0)
+            list->tail = new_element;
+        
+        new_element->next = list->head;
+        list->head = new_element;
+    } else {
+        if (element->next == NULL)
+            list->tail = new_element;
+
+        new_element->next = element->next;
+        element->next = new_element;
+    }
+
+    list->size++;
+
+    return 0;
+}
+
+int list_rem_next(List *list, ListElmt *element, void **data) {
+    ListElmt *old_element;
+
+    if (list_size(list) == 0)
+        return -1;
+
+    if (element == NULL) {
+        *data = list->head->data;
+        old_element = list->head;
+        list->head = list->head->next;
+
+        if (list_size(list) == 1)
+            list->tail = NULL;
+    } else {
+        if (element->next == NULL)
+            return -1;
+        
+        old_element = element->next;
+        *data = old_element->data;
+        element->next = old_element->next;
+
+        if (element->next == NULL)
+            list->tail = element;
+    }
+
+    free(old_element);
+    list->size--;
+
+    return 0;
+}
---
+++ src/list/list.h
@@ -0,0 +1,71 @@
+#ifndef LIST_H
+#define LIST_H
+
+#include <stdlib.h>
+
+/// @brief Estrutura para elementos da lista encadeada
+typedef struct ListElmt {
+    void *data;
+    struct ListElmt *next;
+} ListElmt;
+
+/// @brief Estrutura da Lista Encadeada
+typedef struct List {
+    int size;
+
+    int (*match) (const void *key1, const void *key2);
+    void (*destroy) (void *data);
+
+    ListElmt *head;
+    ListElmt *tail;
+} List; 
+
+/// @brief Inicializa a lista encadeada. Esta operacao tem que ser chamada antes de ser usada por qualquer outra operacao.
+/// @param list lista encadeada 
+/// @param destroy funcao para remover liberar os dados que foram alocados dinamicamente. Para uma lista que contenha dados que nao devem ser liberados, destroy deve ser definido como NULL
+/// @complexity O(1)
+void list_init(List *list, void (*destroy) (void *data));
+
+/// @brief Destroi a lista encadeada.
+/// @param list lista encadeada
+/// @complexity O(n), onde n e o numero de elementos da lista
+void list_destroy(List *list);
+
+/// @brief Insere um elemento na lista.
+/// @param list lista encadeada
+/// @param element sentinela que ajuda inserir o dado na lista. Se element e NULL, o novo elemento e inserido na cabeca da lista 
+/// @param data dado a ser inserido na lista. E inserido logo apos element
+/// @return 0 se a insercao de elemento e bem sucedida, do contrario, -1
+/// @complexity O(1)
+int list_ins_next(List *list, ListElmt *element, const void *data);
+
+/// @brief 
+/// @param list lista encadeada
+/// @param element sentinela que ajuda remover o dado na lista. Se element e NULL, o novo elemento e inserido na cabeca da lista 
+/// @param data dado a ser removido na lista. E removido logo apos element. Na volta, data aponta para os dados armazenados no elemento que foi removido
+/// @return 0 se a insercao de elemento e bem sucedida, do contrario, -1
+/// @complexity O(1)
+int list_rem_next(List *list, ListElmt *element, void **data);
+
+// Macro que avalia o número de elementos na lista encadeada
+#define list_size(list) ((list)->size)
+
+// Macro que avalia o elemento no topo da lista encadeada
+#define list_head(list) ((list)->head)
+
+// Macro que avalia o elemento no final da lista encadeada
+#define list_tail(list) ((list)->tail)
+
+// Macro que determina se o elemento especificado como element está no topo de uma lista encadeada
+#define list_is_head(list, element) ((element) == (list)->head ? 1 : 0)
+
+// Macro que determina se o elemento especificado como element está no final de uma lista encadeada
+#define list_is_tail(element) ((element)->next == NULL ? 1 : 0)
+
+// Macro que avalia os dados armazenados no elemento de uma lista encadeada especificada por elemento
+#define list_data(element) ((element)->data)
+
+// Macro que avalia o elemento de uma lista encadeada seguindo o elemento especificado por element
+#define list_next(element) ((element)->next)
+
+#endif
