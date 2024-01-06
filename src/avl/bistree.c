3af46b3a5624cfc419bf4d3f9f3f3f47606b5574
brenn0219 <brennoaugusto2109@gmail.com>
2024-01-06 14:09:52 -0300

    feat: destroy in the directions

--- src/avl/bistree.c
+++ src/avl/bistree.c
@@ -85,3 +85,55 @@ static void rotate_right(BiTreeNode **node) {
     ((AvlNode *) bitree_data(grandchild))->factor = AVL_BALANCED;
     *node = grandchild;
 }
+
+static void destroy_left(BisTree *tree, BiTreeNode *node) {
+    BiTreeNode **position;
+
+    if (bitree_size(tree) == 0)
+        return;
+
+    if (node == NULL)
+        position = &bitree_root(tree);
+    else
+        position = &bitree_left(node);
+    
+    if (*position != NULL) {
+        bitree_rem_left(tree, *position);
+        bitree_rem_right(tree, *position);
+
+        if (tree->destroy != NULL)
+            tree->destroy(((AvlNode *) bitree_data(*position))->data);
+        
+        free(bitree_data(*position));
+        free(*position);
+        position = NULL;
+
+        tree->size--;
+    }
+}
+
+static void destroy_right(BisTree *tree, BiTreeNode *node) {
+    BiTreeNode **position;
+
+    if (bitree_size(tree) == 0)
+        return;
+
+    if (node == NULL)
+        position = &bitree_root(tree);
+    else
+        position = &bitree_right(node);
+    
+    if (*position != NULL) {
+        bitree_rem_right(tree, *position);
+        bitree_rem_left(tree, *position);
+
+        if (tree->destroy != NULL)
+            tree->destroy(((AvlNode *) bitree_data(*position))->data);
+        
+        free(bitree_data(*position));
+        free(*position);
+        position = NULL;
+
+        tree->size--;
+    }
+}
