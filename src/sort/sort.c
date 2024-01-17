903efc0b3e35598e3a825f73741c1bc17475506d
brenn0219 <brennoaugusto2109@gmail.com>
2024-01-17 19:54:21 -0300

    feat: quicksort

--- src/sort/sort.c
+++ src/sort/sort.c
@@ -24,3 +24,75 @@ int issort(void *list, int size, int esize, int (*compare) (const void *key1, co
 
     return 0;
 }
+
+static int compare_int(const void *int1, const void *int2) {
+    if (*(const int *) int1 > *(const int *) int2) {
+        return 1;
+    } else if (*(const int *) int1 < *(const int *) int2) 
+        return -1;
+    else 
+        return 0;
+}
+
+static int partition(void * list, int size, int esize, int i, int k, int (*compare) (const void *key1, const void *key2)) {
+    char *data = list;
+    void *pval, *temp;
+    int r[3];
+
+    if ((pval = malloc(esize)) == NULL)
+        return -1;
+
+    if ((temp = malloc(esize)) == NULL) {
+        free(pval);
+        return -1;
+    }
+
+    r[0] = (rand() % (k - i + 1)) + i;
+    r[1] = (rand() % (k - i + 1)) + i;
+    r[2] = (rand() % (k - i + 1)) + i;
+
+    issort(r, 3, sizeof(int), compare_int);
+    memcpy(pval, &data[r[1] * esize], esize);
+
+    i--;
+    k++;
+
+    while (1) {
+        do {
+            k--;
+        } while (compare(&data[k * esize], pval) > 0);
+        
+        do {
+            i++;
+        } while (compare(&data[i * esize], pval) < 0);
+
+        if (i >= k)
+            break;
+        else {
+            memcpy(temp, &data[i * esize], esize);
+            memcpy(&data[i * esize], &data[k * esize], esize);
+            memcpy(&data[k * esize], temp, esize);
+        }
+    }
+
+    free(pval);
+    free(temp);
+
+    return 0;
+}
+
+int qksort(void * list, int size, int esize, int i, int k, int (*compare) (const void *key1, const void *key2)) {
+    int j;
+
+    while (i < k) {
+        if ((j = partition(list, size, esize, i, k, compare)) < 0)
+            return -1;
+        
+        if (qksort(list, size, esize, i, j, compare) < 0)
+            return -1;
+
+        i = j + 1;
+    }
+
+    return 0;
+}
--- src/sort/sort.h
+++ src/sort/sort.h
@@ -13,4 +13,15 @@
 /// @complexity O(n²)
 int issort(void *list, int size, int esize, int (*compare) (const void *key1, const void *key2));
 
+/// @brief Ordenacao por QuickSort
+/// @param list array de dados
+/// @param size quantidade total dos dados
+/// @param esize tamanho do dado
+/// @param i define a parte que esta sendo classificada a esquerda, inicialmente deve ser 0
+/// @param k define a parte que esta sendo classificada a direita, inicialmente deve ser size - 1
+/// @param compare funcao de comparacao entre dois dados. Retorna 1 se key1 > key2, 0 key1 == key2 e -1 se key1 < key2
+/// @return 0 se a classsificacao for bem sucedida, do contrario -1
+/// @complexity O(n lg n)
+int qksort(void * list, int size, int esize, int i, int k, int (*compare) (const void *key1, const void *key2));
+
 #endif
